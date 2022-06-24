8d5ec0c238b5d70adaee9307fbe93312059a5863
Bada Jesutobi <88200419+jesutobi-world@users.noreply.github.com>
2022-06-24 23:37:36 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,3 @@
+document.getElementById('name').textContent = 'NAME: Bada Jesutobi Dorcas';
+document.getElementById('height').innerHTML = 'HEIGHT: 163cm';
+document.getElementById('country').innerHTML = 'COUNTRY: Nigeria';
